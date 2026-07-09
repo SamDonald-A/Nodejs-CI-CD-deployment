29fc960a8df513a73bc8c5ea2719c17a7a22c26c
SamDonald-A <samdonaldand@gmail.com>
2026-07-09 12:48:20 +0530

    CI/CD Deployment Backed Remote state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.5"
+  backend "s3" {
+    bucket         = "sam-remote-state-terraform"
+    key            = "terraform/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-lock"
+  }
 
   required_providers {
     aws = {
